768a3788679edb778f9f2b8efc1ff7e6c2fe1517
annababoi <annababoy07@gmail.com>
2022-09-23 14:39:14 +0300

    Initialize project using Create React App

--- src/services/car.service.js
+++ src/services/car.service.js
@@ -4,7 +4,7 @@ import {urls} from "../configs";
 const carService = {
     getAll:()=>axiosServices.get(urls.cars),
     create: (car)=>axiosServices.post(urls.cars, car),
-    getById:(id)=>axiosServices.get(`${urls.cars}/{id}`),
+    getById:(id)=>axiosServices.get(`${urls.cars}/${id}`),
     updateById:(id, car)=>axiosServices.put(`${urls.cars}/${id}`, car),
     deleteById:(id)=>axiosServices.delete(`${urls.cars}/${id}`)
 }
